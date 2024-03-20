43dbcab77378921908c17e13db73b69171a6e6f0
EdbertEL <edbert.eldon.leopard@gmail.com>
2024-03-20 09:41:23 +0700

    Hangman Part 3

--- Hangman.py
+++ Hangman.py
@@ -1,31 +1,32 @@
+def hangman(secretWord):
+    guess = ""
+    lives = 6
 
-word = "Programming"
-guess = ""
-lives = 6
-
-while lives > 0:
-    count = 0
-    for char in word:
-        if char.lower() in guess.lower():
-            print(char, end="")
-        else:
-            print("_", end="")
-            count += 1
-    if count == 0:
-        print("\nThe word is \"", word, "\". You won!", sep="")
-        break
-
-    userinput = input("\nYour guess: ")
-    if len(userinput) != 1:
-        print("You can only guess ONE letter at once!")
-    elif userinput.lower() in guess:
-        print("You already guessed that letter.")
-    else:
-        guess += userinput
-        if userinput.lower() not in word.lower():
-            lives -= 1
-            if lives == 0:
-                print("You lost! Try again next time.")
+    while lives > 0:
+        count = 0
+        for char in secretWord:
+            if char.lower() in guess.lower():
+                print(char, end="")
             else:
-                print("Guess again! You only have {} \u2764\uFE0F  left.".format(lives))
+                print("_", end="")
+                count += 1
+        if count == 0:
+            print("\nThe word is \"", secretWord, "\". You won!", sep="")
+            break
+
+        userinput = input("\nYour guess: ")
+        if len(userinput) != 1:
+            print("You can only guess ONE letter at once!")
+        elif userinput.lower() in guess:
+            print("You already guessed that letter.")
+        else:
+            guess += userinput
+            if userinput.lower() not in secretWord.lower():
+                lives -= 1
+                if lives == 0:
+                    print("You lost! Try again next time.")
+                else:
+                    print("Guess again! You only have {} \u2764\uFE0F  left.".format(lives))
+
+hangman("Programming")
             
